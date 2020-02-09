a7af27f3921de4c6b441647674e92e8fdf721c98
danferian <icanfly654@gmail.com>
2020-02-09 22:09:18 +0700

    init config call and command decision

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"awawe/cmd/server"
+	config "awawe/configuration"
+	"awawe/infrastucture/datastore"
+)
 
+func main() {
+	config.InitializeConfig()
+
+	switch config.GetAppConfig().Command {
+	case "migrate":
+		datastore.MigrateMySQLDatabase(nil)
+	case "server":
+		server.StartServer()
+	default:
+		panic("App command is invalid!")
+	}
 }
