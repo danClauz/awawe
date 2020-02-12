dce148a52c497cd73a57ff303e248c401bd070b0
danferian <icanfly654@gmail.com>
2020-02-12 17:51:14 +0700

    fix migration query password length

--- migrations/20200204152616_create_users_table.up.sql
+++ migrations/20200204152616_create_users_table.up.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS users
     first_name VARCHAR(100)        NOT NULL,
     last_name  VARCHAR(100)        NOT NULL,
     email      VARCHAR(100) UNIQUE NOT NULL,
-    password   VARCHAR(100)        NOT NULL,
+    password   VARCHAR(255)        NOT NULL,
     created_at DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
